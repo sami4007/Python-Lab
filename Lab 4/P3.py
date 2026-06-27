ad7ae4617f085ec0b6e7ab6261e6f1a9b954b509
sami4007 <al.sami88744@gmail.com>
2026-06-27 22:20:18 +0600

    Lab 5 Problem 3

---
+++ Lab 4/P3.py
@@ -0,0 +1,11 @@
+def palindrome(text):
+
+    if text == text[::-1]:
+        print("Palindrome")
+    
+    else:
+        print("Not Palindrome")
+
+word = input("Enter a string: ")
+
+palindrome(word)
