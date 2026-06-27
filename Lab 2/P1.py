db37e38c02c6f1e25bd85710339097834c69a99c
sami4007 <al.sami88744@gmail.com>
2026-06-27 21:35:49 +0600

    Lab 2 Problem 1

--- Lab 2/P1.py
+++ Lab 2/P1.py
@@ -1,12 +1,16 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
 
 print("Before swapping:")
+
 print("a =", a)
+
 print("b =", b)
 
 a, b = b, a
 
 print("After swapping:")
+
 print("a =", a)
+
 print("b =", b)
