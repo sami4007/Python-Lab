5b973692d5d9045a8593ed2cfaf101a74f7a5fde
sami4007 <al.sami88744@gmail.com>
2026-06-27 22:18:24 +0600

    Lab 5 Problem 2

---
+++ Lab 4/P2.py
@@ -0,0 +1,20 @@
+dict = {
+    'V': 10,
+    'VI': 10,
+    'VII': 40,
+    'VIII': 20,
+    'IX': 70,
+    'X': 80,
+    'XI': 40,
+    'XII': 20
+}
+
+frequency = {}
+
+for value in dict.values():
+    if value in frequency:
+        frequency[value] += 1
+    else:
+        frequency[value] = 1
+
+print(frequency)
