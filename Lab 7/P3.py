95fb36197cfa3caf44929b407ef158bb2b93b499
sami4007 <al.sami88744@gmail.com>
2026-08-08 01:23:41 +0600

    Lab 7 Problem 3

---
+++ Lab 7/P3.py
@@ -0,0 +1,11 @@
+list = [10, 20, 30, 40, 50]
+
+try:
+    index = int(input("Enter the index: "))
+    print("Element:", list[index])
+
+except IndexError:
+    print("Error: Index is out of range.")
+
+except TypeError:
+    print("Error: Index must be an integer.")
