3258a76c1cded5d3ec4371691a9e8eb0d0fc0e39
sami4007 <al.sami88744@gmail.com>
2026-08-22 21:45:17 +0600

    Lab 11 Problem 4

---
+++ Lab 11/P4.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = np.array([10, 20, 30])
+y = np.array([2, 5, 10])
+
+print("Sum:", np.add(x, y))
+print("Subtraction:", np.subtract(x, y))
+print("Multiplication:", np.multiply(x, y))
+print("Division:", np.divide(x, y))
